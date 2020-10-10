f1f4b62b3a9a4946ba6610a580e17e7e1a29f856
khetwal-om <khetwalom@gmail.com>
2020-10-10 16:55:39 +0530

    02 Navbar is working

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { BrowserRouter } from 'react-router-dom'
+import { Navbar } from './components'
+import GlobalStyle from './globalStyles'
+
+
+
+
+
+function App() {
+    return (
+        <BrowserRouter>
+         <GlobalStyle/>
+           <Navbar /> 
+
+        </BrowserRouter>
+          
+    )
+}
+
+export default App
